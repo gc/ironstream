b7160954b4718e499a820a329fa78b36bff3d672
gc <30398469+gc@users.noreply.github.com>
2025-03-28 22:50:18 +1100

    Remove files

--- build.sh
+++
@@ -1,8 +0,0 @@
-cargo fmt
-cargo clippy
-cargo build --release
-docker build -t gc261/ironstream:latest .
-truncate -s 0 build.txt
-date --utc >> build.txt
-docker inspect --format='{{.Id}}' gc261/ironstream:latest >> build.txt
-# docker push gc261/ironstream:latest
